1c926c09ba3f2cb1f0733b5b3c776145deec705c
anonymous <anonymous@localhost.localdomain>
2019-09-28 15:28:29 +0530

    implement express

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-console.log('API App Configuration');
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send({
+    message: 'Hello World'
+  })
+});
+
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`Listining at port no: ${port} ...`));
