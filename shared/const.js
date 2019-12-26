bc93ede58507e616a3ab698d05e4e593285c99dc
suvadip <dip.suvadip@gmail.com>
2019-12-27 01:12:51 +0530

    error handeling

--- shared/const.js
+++ shared/const.js
@@ -1,9 +1,11 @@
 /** Mongo Error Code */
 const ERR_11000 = 11000;
 const ERR_11000_MESSAGE = 'Duplicate Retailer Id';
+const ERR_FILESTRUCTURE = 'Wrong! file format';
 
 
 module.exports = {
   ERR_11000,
-  ERR_11000_MESSAGE
+  ERR_11000_MESSAGE,
+  ERR_FILESTRUCTURE
 };
--- shared/error.js
+++ shared/error.js
@@ -1,6 +1,7 @@
 const {
   ERR_11000,
-  ERR_11000_MESSAGE
+  ERR_11000_MESSAGE,
+  ERR_FILESTRUCTURE
 } = require('../shared/const');
 
 function createErrorJSON(errCode, errMessage) {
@@ -16,6 +17,8 @@ exports.sendError = (errCode, errMessage = null) => {
   switch (errCode) {
   case ERR_11000:
     return createErrorJSON(errCode, errMessage || ERR_11000_MESSAGE);
+  case ERR_FILESTRUCTURE:
+    return createErrorJSON(errCode, errMessage || ERR_FILESTRUCTURE);
   default:
     return createErrorJSON(errCode, errMessage || 'Unkown error Occured');
   }
